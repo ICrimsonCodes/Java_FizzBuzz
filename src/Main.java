0e2c386206d46cab26c77254f57c5edf21b18b4d
CrimsonCodes <ICrimsonCodes@gmail.com>
2023-02-01 00:58:45 +0500

    add comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // create scanner
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a Number: ");
         int x = scanner.nextInt();
