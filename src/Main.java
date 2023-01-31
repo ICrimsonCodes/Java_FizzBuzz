6f229ecac47baac959e36316f76be74e16a4db06
CrimsonCodes <ICrimsonCodes@gmail.com>
2023-02-01 00:56:55 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int x = scanner.nextInt();
+        for (int i = 0; i <= x; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+        }
+    }
+}
